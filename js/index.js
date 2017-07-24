03b2bfdebc3560320eb9d24c06bcce66b4bdf4eb
EthanLam <ethan__lam@163.com>
2017-07-24 18:06:14 +0800

    add

---
+++ js/index.js
@@ -0,0 +1,32 @@
+avalon.ready(function() {
+  var vm = avalon.define({
+    $id: 'carousel',
+    cur: 0,
+    move: false,
+    imgsList: [{
+      url: './imgs/01.jpg'
+    }, {
+      url: './imgs/02.jpg'
+    }, {
+      url: './imgs/03.jpg'
+    }],
+    "left": function() {
+      vm.move = !vm.move
+    },
+    "loop": function() {
+      var cur = vm.imgsList.length
+      if (vm.cur) {
+        vm.cur--
+      } else {
+        vm.cur = cur
+      }
+    },
+    "jump": function(idx) {
+      vm.cur = idx
+    },
+  })
+  setInterval(function() {
+    vm.loop()
+  }, 3000)
+  avalon.scan(document.body)
+})
